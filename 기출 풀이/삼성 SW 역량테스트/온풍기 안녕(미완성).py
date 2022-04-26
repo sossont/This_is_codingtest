2c29cac4d6a2423f2da0c7f999844dd763c736cb
Hwanu Jung <sossont1@gmail.com>
2022-04-27 02:13:15 +0900

    [2022-04-26] 삼성 SW 역량테스트 준비

--- 기출 풀이/삼성 SW 역량테스트/온풍기 안녕(미완성).py
+++ 기출 풀이/삼성 SW 역량테스트/온풍기 안녕(미완성).py
@@ -1,3 +1,256 @@
+# 조사하는 칸의 온도가 K이상이 되었는 지 검사
+# 이 문제의 쟁점 : 온풍기를 어떻게 구현할 것인가
+# 나는 상 하 좌 우 온풍기를 다 따로 뒀는데, 이 방법이 맞나?
+# 2시간 컷
+# 큐를 이용하는게 정말 좋은 방법 같다.
+room = []   # 방 정보
+R,C,K = map(int,input().split())
+for _ in range(R):
+    room.append(list(map(int,input().split())))
+
+W = int(input())
+room_wall = []   # 벽 정보
+for _ in range(W):
+    room_wall.append(list(map(int,input().split())))
+
+wall = [[[] for _ in range(C)]  for _ in range(R)]
+for x,y,t in room_wall:
+    wall[x-1][y-1].append(t)
+
+
+### 벽은 (x,y), (x-1,y) 사이 가로벽, (x,y), (x,y+1) 사이 세로벽을 x,y 기준으로 세워진다.
+machines = []   # 온풍기 들어 있는 칸
+target = [] # 온도 조사해야 하는칸
+for i in range(R):
+    for j in range(C):
+        if room[i][j] == 5:
+            target.append([i,j])
+        elif room[i][j] != 0:
+            machines.append([i,j,room[i][j]])
+
+        room[i][j] = 0
+
+# --- 입력 끝 ---
+# 크게 나눠보면
+# 1. 온풍기에서 바람 나오는 함수
+# 2. 온도 조절
+# 3. 바깥쪽 칸 온도 감소
+
+def check(x,y):
+    if 0<=x<R and 0<=y<C:
+        return True
+    return False
+
+# [start_x][start_y] : 온풍기가 놓여 있는 장소
+def right(start_x, start_y):
+    global room
+    q = [[start_x, start_y + 1, 5]]
+    isvisited = [[False] * C for _ in range(R)]
+    while q:
+        x,y,t = q.pop(0)
+        if isvisited[x][y]:
+            continue
+
+        isvisited[x][y] = True
+        room[x][y] += t
+
+        if t <= 1:
+            continue
+
+        # 바로 오른쪽
+        if check(x,y+1) and 1 not in wall[x][y]:
+            q.append([x,y+1,t-1])
+
+        # 위에 오른쪽
+        if check(x-1,y+1):
+            if 0 not in wall[x][y] and 1 not in wall[x-1][y]:
+                q.append([x-1,y+1,t-1])
+
+        # 아래 오른쪽
+        if check(x+1,y+1):
+            if 0 not in wall[x+1][y] and 1 not in wall[x+1][y]:
+                q.append([x+1,y+1,t-1])
+
+def left(start_x, start_y):
+    global room
+    q = [[start_x, start_y - 1, 5]]
+    isvisited = [[False] * C for _ in range(R)]
+    while q:
+        x, y, t = q.pop(0)
+        if isvisited[x][y]:
+            continue
+
+        isvisited[x][y] = True
+        room[x][y] += t
+
+        if t <= 1:
+            continue
+
+        if check(x,y-1):    # 바로 왼쪽
+            if 1 not in wall[x][y-1]:
+                q.append([x,y-1,t-1])
+
+        if check(x-1,y-1):  # 왼쪽 위에
+            if 0 not in wall[x][y] and 1 not in wall[x-1][y-1]:
+                q.append([x-1,y-1,t-1])
+
+        if check(x+1,y-1):  # 왼쪽 아래
+            if not 0 in wall[x+1][y] and 1 not in wall[x+1][y-1]:
+                q.append([x+1,y-1,t-1])
+
+
+def up(start_x, start_y):
+    global room
+    q = [[start_x -1, start_y, 5]]
+    isvisited = [[False] * C for _ in range(R)]
+    while q:
+        x, y, t = q.pop(0)
+
+        if isvisited[x][y]:
+            continue
+
+        isvisited[x][y] = True
+
+        room[x][y] += t
+        if t == 1:
+            continue
+
+        # 바로 위에
+        if check(x-1,y):
+            if 0 not in wall[x][y]:
+                q.append([x-1,y,t-1])
+
+        # 위에 왼쪽
+        if check(x-1,y-1):
+            if 0 not in wall[x][y-1] and 1 not in wall[x][y-1]:
+                q.append([x-1,y-1,t-1])
+
+        # 위에 오른쪽
+        if check(x-1,y+1):
+            if 0 not in wall[x][y+1] and 1 not in wall[x][y]:
+                q.append([x-1,y+1,t-1])
+
+def down(start_x, start_y):
+    global room
+    q = [[start_x + 1, start_y, 5]]
+    isvisited = [[False] * C for _ in range(R)]
+    while q:
+        x,y,t = q.pop(0)
+        if isvisited[x][y]:
+            continue
+
+        isvisited[x][y] = True
+        room[x][y] += t
+
+
+        if t <= 1:
+            continue
+
+        # 바로 아래쪽
+        if check(x+1,y):
+            if 0 not in wall[x+1][y]:
+                q.append([x+1,y,t-1])
+
+        # 아래 왼쪽
+        if check(x+1,y-1):
+            if 1 not in wall[x][y-1] and 0 not in wall[x+1][y-1]:
+                q.append([x+1,y-1,t-1])
+
+        # 아래 오른쪽
+
+        if check(x+1,y+1):
+            if 1 not in wall[x][y] and 0 not in wall[x+1][y+1]:
+                q.append([x+1,y+1,t-1])
+
+def adjust():
+    global R,C
+    delta = [[-1,0],[1,0],[0,1],[0,-1]] # 위 아 오 왼
+    adjust_list = []
+
+    for xx in range(R):
+        for yy in range(C):
+            for d in range(len(delta)):
+                nx = xx + delta[d][0]
+                ny = yy + delta[d][1]
+
+                if not check(nx,ny):
+                    continue
+
+                sub = (room[xx][yy] - room[nx][ny]) // 4
+                if d == 0 and 0 in wall[xx][yy]:
+                    continue
+
+                if d == 1 and 0 in wall[nx][ny]:
+                    continue
+
+                if d == 2 and 1 in wall[xx][yy]:
+                    continue
+
+                if d == 3 and 1 in wall[nx][ny]:
+                    continue
+
+                if sub > 0:
+                    adjust_list.append([xx,yy,-sub])
+                    adjust_list.append([nx,ny,sub])
+
+    while adjust_list:
+        x,y,s = adjust_list.pop()
+        room[x][y] += s
+
+    for xx in range(R):
+        if room[xx][0] > 0:
+            room[xx][0] -= 1
+        if room[xx][C-1] > 0:
+            room[xx][C-1] -= 1
+
+    for yy in range(1,C-1):
+        if room[0][yy] > 0:
+            room[0][yy] -= 1
+        if room[R-1][yy] > 0:
+            room[R-1][yy] -= 1
+
+
+
+def survey():
+    global target
+    global K
+    global room
+    for x,y in target:
+
+        if room[x][y] < K:
+            return False
+
+    return True
+
+
+answer = 0
+while True:
+    answer += 1
+    for init_x, init_y, d in machines:
+        if d == 1:
+            right(init_x,init_y)
+        elif d == 2:
+            left(init_x, init_y)
+        elif d == 3:
+            up(init_x, init_y)
+        elif d == 4:
+            down(init_x,init_y)
+    adjust()
+    if survey():
+        break
+
+    if answer > 100:
+        answer = 101
+        break
+
+if answer > 100:
+    answer = 101
+
+print(answer)
+
+
+
+
 ## ----- 테케는 다 통과하는데 틀리는 코드 -----
 
 # (x,y)의 온도가 k만큼 상승하면,
