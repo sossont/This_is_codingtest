81c07c91de8d2d747aef06c466d5a45c4155150e
Hwanu Jung <sossont1@gmail.com>
2022-04-29 15:53:12 +0900

    [2022-04-29] 삼성 SW 역량테스트 준비

--- 기출 풀이/삼성 SW 역량테스트/아기상어.py
+++ 기출 풀이/삼성 SW 역량테스트/아기상어.py
@@ -1,3 +1,78 @@
+# 이제는 27분 컷..
+import heapq
+
+INF = 987654321
+N = int(input())
+graph = []
+for _ in range(N):
+    graph.append(list(map(int,input().split())))
+
+shark_x, shark_y = 0,0
+shark_size = 2
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 9:
+            shark_x,shark_y = j,i
+            graph[i][j] = 0
+            break
+
+def check(x,y):
+    return 0<=x<N and 0<=y<N
+
+def dijkstra(sx,sy):
+    q = []
+    global N
+    global graph
+    distance = [[INF] * N for _ in range(N)]
+    distance[sy][sx] = 0
+    heapq.heappush(q, [0,sy,sx])
+    delta = [[-1,0],[1,0],[0,1],[0,-1]]
+    while q:
+        cost, y, x = heapq.heappop(q)
+        for d in delta:
+            nx = x + d[0]
+            ny = y + d[1]
+            if not check(nx,ny):
+                continue
+
+            if distance[ny][nx] > cost + 1 and shark_size >= graph[ny][nx]:
+                distance[ny][nx] = cost+1
+                heapq.heappush(q, [cost+1,ny,nx])
+
+    # 그러면 x,y 기준으로 거리가 나올 것
+    ret = []
+    for y in range(N):
+        for x in range(N):
+            if graph[y][x] != 0 and graph[y][x] < shark_size:
+                heapq.heappush(ret,[distance[y][x], y, x])
+
+    return ret
+
+answer = 0
+eat_count = 0
+while True:
+    arr = dijkstra(shark_x, shark_y)
+    if len(arr) == 0:   # 잡아먹을 물고기가 없는 경우
+        break
+    # 잡아먹은 경우
+    dist,gy,gx = heapq.heappop(arr)
+    if dist == INF:
+        break
+    # 그 좌표 물고기 없애주고
+    graph[gy][gx] = 0
+
+    # 상어를 그좌표로 옮겨주고
+    shark_x, shark_y = gx, gy
+    # 먹은 개수 카운트 늘려주고, 사이즈 체크한다.
+    eat_count += 1
+    if eat_count == shark_size:
+        shark_size += 1
+        eat_count = 0
+
+    answer += dist
+
+print(answer)
+### =========
 from collections import deque
 
 # 가장 큰 관건 -> 경로 찾는 것
